83b5bd912ef99ae5b6f3a9ae53f3ae99a1c4ed42
NPH82 <nich1982@gmail.com>
2017-08-19 17:32:16 -0400

    initial commit

---
+++ keys.js
@@ -0,0 +1,8 @@
+console.log("this is loaded");
+
+exports.twitterkeys = {
+	consumer_key: "PqVOOkImRKmTxDNimZbTXle6r",
+	consumer_secret: "tr7iDUJxxxFejG0bPpdRuHbe85BQKoRj90M4W0bR0Gkk9C3N3m",
+	access_token_key: "898995324387295232-UdEagckt3EPDczNoROTQg09kMnnfs9R",
+	acces_token_secret: "898995324387295232-UdEagckt3EPDczNoROTQg09kMnnfs9R",
+}
---
+++ liri.js
@@ -0,0 +1,32 @@
+var keyGrab = grab data from key.js;
+
+//commands to understand:
+
+//my-tweets
+//show last 2o tweets
+
+
+
+//spotify-this-song
+//show Artist
+//show the song's name
+//A preview link of the song from Spotify
+//The album the song is from
+//If no song shown, Default to "the Sign" Ace of Base
+
+
+
+//movie-this
+//show title
+//year movie came out
+//IMDB rating
+//Rotten tomatoes Rating
+//Country where movie was produced
+//Language of Movie
+//Plot of movie
+//Actors
+//if no movie typed in, default to "Mr. Nobody"
+//API Key: 
+
+
+//do-what-it-says
