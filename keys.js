d1a05ac7a91e560c10ad297408545ab5c94adbca
NPH82 <nich1982@gmail.com>
2017-08-21 19:23:16 -0400

    movie-this started

--- keys.js
+++ keys.js
@@ -4,5 +4,10 @@ exports.twitterkeys = {
 	consumer_key: "PqVOOkImRKmTxDNimZbTXle6r",
 	consumer_secret: "tr7iDUJxxxFejG0bPpdRuHbe85BQKoRj90M4W0bR0Gkk9C3N3m",
 	access_token_key: "898995324387295232-UdEagckt3EPDczNoROTQg09kMnnfs9R",
-	acces_token_secret: "898995324387295232-UdEagckt3EPDczNoROTQg09kMnnfs9R",
+	access_token_secret: "gqaGssKBX9XT5do53KmXOAFoyjQBh7r4wD5R2W67gKvDk",
+};
+
+exports.spotifykeys = {
+	id: "936a8be08f694cfd9d918de9f9b9ec58",
+	secret: "f5769cdf33f840ecb38958ac6964d195"
 }
--- liri.js
+++ liri.js
@@ -1,14 +1,57 @@
-var keyGrab = grab data from key.js;
+
+var twitter = require('twitter'),
+	request = require('request'),
+	keys = require('./keys.js');
 
 //commands to understand:
 
 //my-tweets
+
+// var client = new twitter({
+// 	"consumer_key": keys.twitterkeys.consumer_key,
+// 	"consumer_secret": keys.twitterkeys.consumer_secret,
+// 	"access_token_key": keys.twitterkeys.access_token_key,
+// 	"access_token_secret": keys.twitterkeys.access_token_secret
+// 	});
+
+// request('https://api.twitter.com/1.1/search/tweets.json', function(error, response, body) {
+
+
+// 	if (error) {
+// 	console.log("hello")
+// 	console.log(response.statusCode)
+// 	console.log(response);
+// 	console.log(body);
+// 	}
+//  });
+
+
 //show last 2o tweets
+//show when they were createds
 
 
 
 //spotify-this-song
+//search: function({ type: 'artist OR album OR track', query: 'My search query', limit: 20 }, callback);
+
+// var Spotify = require('node-spotify-api');
+// var searchSpotify = process.argv[2];
+
+// var spotify = new Spotify({
+// 	id: keys.spotifykeys.id,
+// 	secret: keys.spotifykeys.secret
+// });
+
+// spotify
+// 	.request('http://api.spotify.com/v1/search?q=' + searchSpotify + '&type=track,album,artist&limit=10')
+// 	.then(function(data) {
+// 		console.log(data);
+// 	})
+// 	.catch(function(err) {
+// 		console.error('error occured: ' + err);
+// }) ;
 //show Artist
+
 //show the song's name
 //A preview link of the song from Spotify
 //The album the song is from
@@ -17,6 +60,27 @@ var keyGrab = grab data from key.js;
 
 
 //movie-this
+var movie = process.argv[2];
+var apiKey = "40e9cece";
+var queryUrl ="http://www.omdbapi.com/?apikey=" + apiKey + "&t=" + movie + "&y=&plot=short";
+
+console.log(queryUrl);
+
+request(queryUrl, function(error, response, body) {
+
+	if (!error && response.statusCode === 200) {
+
+		console.log(JSON.parse(body).Title);
+		console.log(JSON.parse(body).Year);
+		console.log(JSON.parse(body).imdbRating);
+		console.log(JSON.parse(body).Ratings[1].Value);
+		console.log(JSON.parse(body).Country);
+		console.log(JSON.parse(body).Plot);
+		console.log(JSON.parse(body).Actors);
+	}
+});
+
+
 //show title
 //year movie came out
 //IMDB rating
