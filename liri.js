ad5fddd9eb665bde00765df7ce014dec5e88cd6f
NPH82 <nich1982@gmail.com>
2017-08-23 20:27:47 -0400

    working on spotify

--- liri.js
+++ liri.js
@@ -1,5 +1,6 @@
 
 var twitter = require('twitter'),
+	spotify = require('spotify'),
 	request = require('request'),
 	keys = require('./keys.js');
 
@@ -34,30 +35,51 @@ var twitter = require('twitter'),
 //spotify-this-song
 //search: function({ type: 'artist OR album OR track', query: 'My search query', limit: 20 }, callback);
 
-// var Spotify = require('node-spotify-api');
-// var searchSpotify = process.argv[2];
+var searchSpotify = process.argv[2];
 // var queryUrl = "http://api.spotify.com/v1/search?q=" + searchSpotify + "&type=track,album,artist&limit=10&access_token=BQC69iUx57uKqyDC72OnWba9xBgA0n2QU--7omYXuhGgpRqAMSEH-3SFjLvhZhbG7ywZ_sq14JNgcRo5p5PadEQwdtVxaQ2NbFIEOM4mjWOURwFeD8DBMdkRFgSv6F6JbD_p3yxqVxI";
 
+var client_id = keys.spotifykeys.id; 
+var client_secret = keys.spotifykeys.secret; 
 
-// request(queryUrl, function(error, response, body) {
 
-// 	if(!error && response.statusCode === 200) {
-// 		console.log(JSON.parse(body).artists[1].name);
+
+// your application requests authorization
+var authOptions = {
+  url: 'https://accounts.spotify.com/api/token',
+  headers: {
+    'Authorization': 'Basic ' + (new Buffer(client_id + ':' + client_secret).toString('base64'))
+  },
+  form: {
+    grant_type: 'client_credentials'
+  },
+  json: true
+};
+
+request.post(authOptions, function(error, response, body) {
+  if (!error && response.statusCode === 200) {
+  		console.log(response.statusCode);
+    // use the access token to access the Spotify Web API
+    var token = body.access_token;
+    var options = {
+      url: 'https://api.spotify.com/v1/me',
+      headers: {
+        'Authorization': 'Bearer ' + token
+      },
+      json: true
+    };
+    spotify.get(options, function(error, response, body) {
+      console.log(body);
+    });
+  }
+});
+
+// spotify.search({type: 'artist, album, track', query: searchSpotify }, function(err, data){
+// 	if(err) {
+// 		console.log(err);
+// 	} else {
+// 		console.log(data);
 // 	}
 // });
-// var spotify = new Spotify({
-// 	id: keys.spotifykeys.id,
-// 	secret: keys.spotifykeys.secret
-// });
-
-//spotify
-// 	.request('http://api.spotify.com/v1/search?q=' + searchSpotify + '&type=track,album,artist&limit=10')
-// 	.then(function(data) {
-// 		console.log(data);
-// 	})
-// 	.catch(function(err) {
-// 		console.error('error occured: ' + err);
-// }) ;
 
 //show Artist
 
